2ed60492ec1076d8e5d5658ea58272101bb487f4
Suhas Anand Balagar <suhasbalagar@gmail.com>
2022-05-17 08:12:29 -0700

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL='ec2-13-56-250-148.us-west-1.compute.amazonaws.com';
+export const BACKEND_URL='http://ec2-13-56-250-148.us-west-1.compute.amazonaws.com';
 export const BACKEND_PORT='5000';
